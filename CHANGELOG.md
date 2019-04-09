f6f601ee3f376bcc4cf44d707ef7c4cdbf9ddcdd
Thomas Schulze <tschulze@codemonauts.com>
2019-04-09 10:26:38 +0200

    add console command to generate thumbs without job/queues

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,11 @@
 # Craft AWS Plugin Changelog
 
-## 1.0.0-beta.2 - 2019-04-08
+## Unreleased
+### Added
+- Console command to generate thumbs without job/queues
 ### Fixed
-- If resourceRevision was a clousure, an error occured.
+- If resourceRevision was a clousure, an error occured
+- Fix AssetManager by adding S3 functions as trait
 
 ## 1.0.0-beta.1 - 2019-04-06
 ### Added
--- src/console/controllers/ThumbsController.php
+++ src/console/controllers/ThumbsController.php
@@ -63,4 +63,57 @@ class ThumbsController extends Controller
         $this->stdout('Dispatched '.$count.' jobs.'.PHP_EOL, Console::FG_GREEN);
         return ExitCode::OK;
     }
+
+    public function actionGenerateLive($all = false, $assetId = null): int
+    {
+        if (!Aws::getInstance()->getSettings()->thumbnailsOnBucket) {
+            $this->stderr('Storing thumbnails into a bucket is not enabled!'.PHP_EOL, Console::FG_RED);
+            return ExitCode::OK;
+        }
+
+        if ($all === false && $assetId === null)
+        {
+            $this->stderr('You forgot to say what to do!'.PHP_EOL, Console::FG_RED);
+            return ExitCode::OK;
+        }
+
+        if ($all) {
+            $assets = Asset::find()
+                ->anyStatus()
+                ->ids();
+        } else {
+            $assets = [$assetId];
+        }
+
+        $count = count($assets);
+        $counter = 0;
+
+        $baseUrl = Aws::getInstance()->getSettings()->thumbnailsBaseUrl;
+
+        Console::startProgress($counter, $count, 'Generating thumbs');
+
+        foreach ($assets as $assetId) {
+
+            $asset = Craft::$app->assets->getAssetById($assetId);
+
+            $meta = Aws::getInstance()->assets->createAllThumbSizes($asset);
+
+            $cachedMeta = [
+                'baseUrl' => $baseUrl . '/' . $meta['path'],
+                'ext' => $meta['ext'],
+            ];
+
+            Craft::$app->cache->set('aws-thumb-' . $asset->id, $cachedMeta, 0);
+
+            unset($asset);
+            unset($meta);
+
+            Console::updateProgress(++$counter, $count);
+        }
+
+        Console::endProgress(true, false);
+
+        $this->stdout('Generated thumbs for '.$count.' assets.'.PHP_EOL, Console::FG_GREEN);
+        return ExitCode::OK;
+    }
 }
