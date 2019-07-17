fd59f4c651cd0b58e96a47a23f15baf53068252d
Thomas Schulze <tschulze@codemonauts.com>
2019-07-17 11:06:03 +0200

    add browser type detection

--- src/variables/AwsVariables.php
+++ src/variables/AwsVariables.php
@@ -10,4 +10,9 @@ class AwsVariables
     {
         return Aws::getInstance()->cloudfront->isMobileBrowser($tabletAsMobile);
     }
+
+    public function getBrowserType()
+    {
+        return Aws::getInstance()->cloudfront->getBrowserType();
+    }
 }
